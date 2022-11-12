ff73bc496f6fd0777e9da372320c0c6645e8e236
Rodrigo Silva <rodrigopsilva.profissional@gmail.com>
2022-11-11 22:24:20 -0300

    condicoes

---
+++ estudos/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 10
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { //condição siples
+    console.log(`Você vai ser multado!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`) 
